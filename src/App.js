b8544c9a7a1394228d0dceded0bd6d5ed8c9b48e
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-07-09 20:32:05 +0530

    test api called

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [products,setproducts] = useState([])
+
+  const fetchdata = async () => {
+    const response = await axios.get("https://fakestoreapi.com/products")
+    setproducts(response.data)
+  }
+  useEffect(()=>{
+    fetchdata()
+  },[]);
+
   return (
     <div className="App">
-       <h1>My name is Chanidu</h1>
+
+      {products.map((product)=>(
+        <div key={product.id} className='product'>
+          <h2>{product.title}</h2>
+          <img src={product.image} alt={product.title} />
+          <p>{product.description}</p>
+          <h3>$ {product.price}</h3>
+        </div>
+      ))}
+     
     </div>
   );
 }
